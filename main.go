aad73bba8f8942fde8c88b39a58961f5acd99a26
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-02-18 21:16:19 +0000

    Setup Request And Response For Resolve

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import "github.com/gofiber/fiber"
 
+func setupRoutes(app *fiber.App) {
+}
+
 func main() {
 	app := fiber.New()
 
---
+++ routes/shorten.go
@@ -0,0 +1,17 @@
+package routes
+
+import "time"
+
+type request struct {
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
+}
+
+type response struct {
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  int           `json:"rateLimit"`
+	XRateLimitReset time.Duration `json:"rateLimitReset"`
+}
