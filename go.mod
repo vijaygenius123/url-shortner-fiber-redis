099da00daf22544ba77d32c89dbefa251598cd71
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-02-22 19:53:36 +0000

    Added ResolveURL

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module url-shortner-fiber-redis
 go 1.16
 
 require (
-	github.com/gofiber/fiber v1.14.6
+	github.com/go-redis/redis/v8 v8.11.4
 	github.com/gofiber/fiber/v2 v2.27.0
 	github.com/joho/godotenv v1.4.0
 )
--- routes/resolve.go
+++ routes/resolve.go
@@ -1,1 +1,30 @@
 package routes
+
+import (
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"url-shortner-fiber-redis/database"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "short url not found in the database"})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Cannot connect to DB",
+		})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	_ = rInr.Incr(database.Ctx, "counter")
+	return c.Redirect(value, fiber.StatusPermanentRedirect)
+}
