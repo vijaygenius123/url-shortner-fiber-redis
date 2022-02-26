c3d5c74d0074a855b3060f2dc039997204c9e7e5
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-02-26 19:23:55 +0000

    Save CustomShort Url if it does not exist

--- routes/shorten.go
+++ routes/shorten.go
@@ -54,6 +54,31 @@ func ShortenUrl(c *fiber.Ctx) error {
 
 	body.URL = helpers.EnforceHTTP(body.URL)
 
+	var id string
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{"error": "short URL is already in use"})
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = 24
+	}
+
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Unable to connect to server"})
+	}
+
 	r2.Decr(database.Ctx, c.IP())
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success"})
